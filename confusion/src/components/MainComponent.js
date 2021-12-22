bb78f8ee63efa1a4352181d1a5150538372b8764
remter <krch.addanki@gmail.com>
2021-12-22 05:33:07 -0800

    Single page Applications

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,21 +9,29 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './HomeComponent';
-
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 
 class Main extends Component{
   constructor(props){
     super(props);
     this.state={
-      dishes:DISHES,
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     }
   }
 
   render(){
     const HomePage = () => {
       return(
-          <Home 
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -34,6 +42,7 @@ class Main extends Component{
         <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path='/contactus' component={Contact} />
             <Redirect to="/home" />
         </Switch>
         <Footer />
